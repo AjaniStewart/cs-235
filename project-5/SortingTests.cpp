ffa44345169f583c8e56d33ea56d234054e87470
Ajani Stewart <theamjaha@gmail.com>
2018-12-11 09:42:14 -0500

    finished the project. gradescope didnt complain so i guess im done.

--- project-5/SortingTests.cpp
+++ project-5/SortingTests.cpp
@@ -4,23 +4,23 @@
 #include <chrono>     // for timing functions
 #include <cstdlib>    // for rand and srand
 
-#include "SortingTests.h"
+#include "SortingTests.hpp"
+
+using namespace std::chrono;
 
 /*****************************************************************/
 /********************* SORTING FUNCTIONS *************************/
 /*****************************************************************/
 
 void selectionSort(int a[], size_t size) {
-  for (size_t i = 0; i < size-1; ++i) {
-    bool swapped = false;
-    for (size_t j = 0; j < size-i-1; ++j) {
-      if (a[j] > a[j+1]){
-        std::swap(a[j],a[j+1]);
-        swapped = true;
+  for (size_t start_index = 0; start_index < size-1; ++start_index) {
+    int smallest_index = start_index;
+    for (size_t current_index = start_index+1; current_index < size; ++current_index) {
+      if (a[current_index] < a[smallest_index]){
+        smallest_index = current_index;
       }
     }
-    if (!swapped)
-      break;
+    std::swap(a[start_index],a[smallest_index]);
   }
 }
 
@@ -76,3 +76,78 @@ void mergeSort(int a[], int from, int to) {
 /*********************************************************************/
 /****************** ARRAY POPULATING FUNCTIONS ***********************/
 /*********************************************************************/
+
+//from learncpp.com
+//https://www.learncpp.com/cpp-tutorial/59-random-number-generation/
+
+int getRandomNumber(int min = 0, int max = 10000) {
+  // static used so that this is calculated once
+  static const double fraction = 1.0 / (RAND_MAX + 1.0);
+  //evenly distribute the random number across the range
+  return min + static_cast<int>((max-min+1)*(std::rand()*fraction));
+}
+
+void generateRandomArray(int values[], size_t size) {
+  //set inital seed value to system clock
+  for (size_t i = 0; i < size; ++i) {
+    values[i] = getRandomNumber();
+  }
+}
+
+void generateAscendingArray(int values[], size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    values[i] = i;
+  }
+}
+
+void generateDescendingArray(int values[], size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    values[i] = size-i;
+  }
+}
+
+void generateLastTenRandomArray(int values[], size_t size) {
+  generateAscendingArray(values,size-10);
+  for (size_t i = size-10; i < size; ++i) {
+    values[i] = getRandomNumber();
+  }
+}
+
+void generateFewRandomArray(int values[], size_t size) {
+  
+  for (size_t i = 0;i < size; ++i) {
+    values[i] = getRandomNumber(0,size/10);
+  }
+}
+
+/*****************************************************************/
+/****************** RUNTIME TEST FUNCTIONS ***********************/
+/*****************************************************************/
+
+double sortTest(void (*sortingFunction)(int a[], size_t size), int values[], size_t size) {
+  double total_time = 0;
+  int* values_copy = values;
+  for (int i = 0; i < 10; ++i) {
+    auto start_time = high_resolution_clock().now();
+    sortingFunction(values_copy,size);
+    auto end_time = high_resolution_clock().now();
+    duration<float,std::micro> run_time = duration_cast<microseconds>(end_time-start_time);
+    total_time += run_time.count();
+    values_copy = values;
+  }
+  return total_time/10.0;
+}
+
+double mergeSortTest(int values[], size_t size) {
+  double total_time = 0;
+  int* values_copy = values;
+  for (int i = 0; i < 10; ++i) {
+    auto start_time = high_resolution_clock().now();
+    mergeSort(values_copy,0,size-1);
+    auto end_time = high_resolution_clock().now();
+    duration<float,std::micro> run_time = duration_cast<microseconds>(end_time-start_time);
+    total_time += run_time.count();
+    values_copy = values;
+  }
+  return total_time/10.0;
+}
--- project-5/tests.cpp
+++ project-5/tests.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-#include "SortingTests.h"
+#include "SortingTests.hpp"
 
 int main()
 {
-  int a[5] = {4,2,5,3,1};
-  mergeSort(a,0,4);
-  int b[5];
+  int a[10000];
+  generateFewRandomArray(a,10000);
+  std::cout << "mergesort took " << mergeSortTest(a,10000) << " micro secs.\n";
+  std::cout << "insertion sort took " << sortTest(insertionSort,a,10000) << " micro secs.\n";
+  std::cout << "selection sort took " << sortTest(selectionSort,a,10000) << " micro secs.\n";
   //std::copy(a,a+2,b);
   
-  for(auto x : a)
-  {
-    std::cout << x << " ";
-  }
+  // for(auto x : a)
+  // {
+  //   std::cout << x << " ";
+  // }
   
   return 0;
 }
